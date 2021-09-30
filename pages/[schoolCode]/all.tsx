b52c535a05a3326785f6cd287fedd95c04afecf8
joonsung.um <joonsung.um@gmail.com>
2021-09-30 15:03:41 +0900

    add table

--- pages/[schoolCode]/all.tsx
+++ pages/[schoolCode]/all.tsx
@@ -2,19 +2,39 @@ import React, { Fragment } from 'react'
 import { NextPage, GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
-import Card from '@mui/material/Card'
+
+import Typography from '@mui/material/Typography'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+
+import { styled } from '@mui/material/styles'
+
 import { timetable } from 'school-info'
 import { NEIS_OPEN_API_KEY } from '../../config'
 
+
+const Cell = styled(TableCell)`
+  padding: 8px 16px;
+`
 
 interface AllClassProps {
   timeTable: any
 }
 
+const getClassContent = (timeTable: any, grade: string, className: string, perio: string) => {
+  return timeTable.map((item: any) => {
+    if (item.GRADE === grade && item.CLASS_NM === className && item.PERIO === perio) {
+      return item.ITRT_CNTNT
+    }
+  })
+}
+
 const AllClass: NextPage<AllClassProps> = ({ timeTable }) => {
-  console.log(timeTable)
   return (
     <Fragment>
       <Head>
@@ -23,74 +43,138 @@ const AllClass: NextPage<AllClassProps> = ({ timeTable }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container maxWidth="xl">
-        <h1>고등학교시간표</h1>
+        <Typography align="center" variant="h5" gutterBottom component="div" mt="10px">
+          남해정보산업고등학교 시간표
+        </Typography>
 
-        <Box sx={{
-          display: 'grid',
-          margin: '0 20px',
-          gridTemplateColumns: 'repeat(3, 1fr)',
-        }}>
-          <Grid container spacing={{ xs: 2, md: 3, lg: 3, xl: 3}}>
-            <Card>
-              {timeTable.map((item: any) => {
-                return item.GRADE === '1' && item.CLASS_NM === '1' && (
-                  <li key={`${item.GRADE}-${item.CLASS_NM}`}>
-                    {item.PERIO}교시: {item.ITRT_CNTNT}
-                  </li>
-                )
-              })}
-            </Card>
-            <Card>
-              {timeTable.map((item: any) => {
-                return item.GRADE === '1' && item.CLASS_NM === '2' && (
-                  <li key={`${item.GRADE}-${item.CLASS_NM}`}>
-                    {item.PERIO}교시: {item.ITRT_CNTNT}
-                  </li>
-                )
-              })}
-            </Card>
-          </Grid>
-          <Grid container spacing={{ xs: 2, md: 3, lg: 3, xl: 3}}>
-            <Card>
-              {timeTable.map((item: any) => {
-                return item.GRADE === '2' && item.CLASS_NM === '1' && (
-                  <li key={`${item.GRADE}-${item.CLASS_NM}`}>
-                    {item.PERIO}교시: {item.ITRT_CNTNT}
-                  </li>
-                )
-              })}
-            </Card>
-            <Card>
-              {timeTable.map((item: any) => {
-                return item.GRADE === '2' && item.CLASS_NM === '2' && (
-                  <li key={`${item.GRADE}-${item.CLASS_NM}`}>
-                    {item.PERIO}교시: {item.ITRT_CNTNT}
-                  </li>
-                )
-              })}
-            </Card>
-          </Grid>
-          <Grid container spacing={{ xs: 2, md: 3, lg: 3, xl: 3}}>
-            <Card>
-              {timeTable.map((item: any) => {
-                return item.GRADE === '3' && item.CLASS_NM === '1' && (
-                  <li key={`${item.GRADE}-${item.CLASS_NM}`}>
-                    {item.PERIO}교시: {item.ITRT_CNTNT}
-                  </li>
-                )
-              })}
-            </Card>
-            <Card>
-              {timeTable.map((item: any) => {
-                return item.GRADE === '3' && item.CLASS_NM === '2' && (
-                  <li key={`${item.GRADE}-${item.CLASS_NM}`}>
-                    {item.PERIO}교시: {item.ITRT_CNTNT}
-                  </li>
-                )
-              })}
-            </Card>
-          </Grid>
-        </Box>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <Cell align="center"></Cell>
+                <Cell align="center">1학년 1반</Cell>
+                <Cell align="center">2학년 1반</Cell>
+                <Cell align="center">3학년 1반</Cell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <Cell align="center">1교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '1', '1')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '1', '1')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '1', '1')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">2교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '1', '2')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '1', '2')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '1', '2')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">3교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '1', '3')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '1', '3')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '1', '3')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">4교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '1', '4')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '1', '4')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '1', '4')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">5교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '1', '5')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '1', '5')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '1', '5')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">6교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '1', '6')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '1', '6')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '1', '6')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">7교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '1', '7')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '1', '7')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '1', '7')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">8교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '1', '8')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '1', '8')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '1', '8')}</Cell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <br />
+
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <Cell align="center"></Cell>
+                <Cell align="center">1학년 2반</Cell>
+                <Cell align="center">2학년 2반</Cell>
+                <Cell align="center">3학년 2반</Cell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <Cell align="center">1교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '2', '1')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '2', '1')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '2', '1')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">2교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '2', '2')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '2', '2')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '2', '2')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">3교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '2', '3')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '2', '3')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '2', '3')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">4교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '2', '4')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '2', '4')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '2', '4')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">5교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '2', '5')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '2', '5')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '2', '5')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">6교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '2', '6')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '2', '6')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '2', '6')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">7교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '2', '7')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '2', '7')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '2', '7')}</Cell>
+              </TableRow>
+              <TableRow>
+                <Cell align="center">8교시</Cell>
+                <Cell align="center">{getClassContent(timeTable, '1', '2', '8')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '2', '2', '8')}</Cell>
+                <Cell align="center">{getClassContent(timeTable, '3', '2', '8')}</Cell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+
       </Container>
     </Fragment>
   )
@@ -118,7 +202,6 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     ALL_TI_YMD: timeCode,
   })
 
-  console.log(timeTable)
   return {
     props: {
       timeTable
